5a2f7be17b528619e1ea885aa956bd20b181ae54
BrunoGabii <brunogaraujoclaudino@gmail.com>
2023-09-20 18:27:51 -0300

    crei um header

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header>Header</header>
+        <aside>Aside</aside>
+        <div className='content'>TESTE</div>
+      </div>
+    </>
+  )
+}
+
+export default App
